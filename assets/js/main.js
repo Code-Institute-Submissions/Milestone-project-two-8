4983ee14d65295f64662ac9498f5d00d9cfb406c
sree0831 <sreelakshmi.passavula@gmail.com>
2020-05-06 22:47:41 +0000

    marker to maps

--- assets/js/main.js
+++ assets/js/main.js
@@ -622,43 +622,18 @@ $('.testimonial-pics img').hover(function(){
 //end of testimony
 
 //maps
+
 var google;
 
-function init() {
-    var myLatlng = new google.maps.LatLng(59.334591, 18.063240);   
-      
-    var mapOptions = {
-        
-        zoom: 7,
-        center: myLatlng,
-        scrollwheel: false,
-        
-    };
+   function initMap() {
 
+  var Stockholm = {lat: 59.334591, lng: 18.063240};
 
-    var mapElement = document.getElementById('map');
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 8, center: Stockholm});
 
-   
-    var map = new google.maps.Map(mapElement, mapOptions);
-    
-    var addresses = ['Stockholm'];
-
-    for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
-            var p = data.results[0].geometry.location
-            var latlng = new google.maps.LatLng(p.lat, p.lng);
-            new google.maps.Marker({
-                position: latlng,
-                map: map,
-                icon: 'images/loc.png'
-            });
-
-        });
-    }
-    
+  var marker = new google.maps.Marker({position: Stockholm, map: map});
 }
-google.maps.event.addDomListener(window, 'load', init);
-
 // end of maps 
 
 //slider
