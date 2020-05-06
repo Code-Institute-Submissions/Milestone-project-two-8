ba3da91432849a27d2552a1da7e161ab381e3e8b
sree0831 <sreelakshmi.passavula@gmail.com>
2020-05-06 20:20:30 +0000

    added marker to map

--- assets/js/google-map.js
+++ assets/js/google-map.js
@@ -1,36 +1,12 @@
 
 var google;
 
-function init() {
-    var myLatlng = new google.maps.LatLng( 59.334591, 18.063240);   
-    var mapOptions = {
-        
-        zoom: 9,
-        center: myLatlng,
-        scrollwheel: true,
-        
-    };
+   function initMap() {
 
+  var Stockholm = {lat: 59.334591, lng: 18.063240};
 
-    var mapElement = document.getElementById('map');
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 8, center: Stockholm});
 
-   
-    var map = new google.maps.Map(mapElement, mapOptions);
-    
-    var addresses = ['Stockholm'];
-
-    for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address=Stockholm&key=AIzaSyB-nNMm3QeOUilp90E8kibDufs-SuoIgak', null, function (data) {
-            var p = data.results[0].geometry.location
-            var latlng = new google.maps.LatLng(p.lat, p.lng);
-            new google.maps.Marker({
-                position: latlng,
-                map: map,
-                icon: 'assets/images/loc.png'
-            });
-
-        });
-    }
-    
+  var marker = new google.maps.Marker({position: Stockholm, map: map});
 }
-google.maps.event.addDomListener(window, 'load', init);
